3e4372fe7b34479b88fef4435736ea5cca21af40
saurabhmaurya45 <tgtsaurabhmaurya45@gmail.com>
2021-06-23 22:00:45 +0530

    Adding more files

---
+++ find_average_of_3_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	/* code */
+	int a,b,c;  //declaration
+	a=10;       //assignment   
+	b=20;       //assignment
+	c=30;       //assignment
+	float avg=(a+b+c)/3;        //calculation
+	printf("%.2f\n",avg );      //throughing output
+	return 0;
+}
